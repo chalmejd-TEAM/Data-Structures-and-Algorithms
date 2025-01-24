964f39af38eba37c6360f907123b5ac489126bdf
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-24 11:38:44 -0600

    Added larger arrays and timing

--- findNemo.py
+++ findNemo.py
@@ -1,8 +1,20 @@
+from datetime import datetime
 nemo = ['nemo']
+everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+large = nemo * 100000
 
 def findNemo(array):
+    count = 0
+    t0 = datetime.now().microsecond
     for i in range(len(array)):
         if(array[i] == 'nemo'):
-            print('Found Nemo')
+            # print('Found Nemo')
+            count = count + 1
+    t1 = datetime.now().microsecond
+    print('Call took:', t1-t0, 'microseconds. Found ', count, 'Nemos')
+    
 
 findNemo(nemo)
+findNemo(everyone)
+findNemo(large)
+
