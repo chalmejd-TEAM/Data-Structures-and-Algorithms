54b6a8c28f73498f89bbfaf316c20e016a56ded7
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-30 13:41:23 -0600

    Added insert, update, and remove first

--- DataStructures/LinkedLists/linkedListIntro.py
+++ DataStructures/LinkedLists/linkedListIntro.py
@@ -50,4 +50,43 @@ def insert(self, data, index):
     
     position = 0 # Set initial position index to 0
     current_node = self.head # Set current_node to head node
-    
+    # While current_node is not None and position is less than the index
+    while (current_node != None and position+1 != index):
+        position = position+1 # Add 1 to postion 
+        current_node = current_node.next # move current node to next node in line
+
+    # If current node exists
+    if current_node != None: 
+        new_node = Node(data) # Create new node with given data
+        new_node.next = current_node.next # Set new node next to current node next
+        current_node.next = new_node # Set current node next to new node
+    else:
+        print("Index is not present") # If current node does not exist print error message
+
+def update(self, val, index):
+    current_node = self.head # Set current node to head node
+    position = 0 # Set initial position index to 0
+    # If position is equal to index, set current node data to provided value
+    if position == index:
+        current_node.data = val
+    # Else start traversing the list until target index is reached
+    else:
+        # Loop through list while current node exists and until position equals provided index
+        while(current_node != None and position != index):
+            position = position+1 # Increment position
+            current_node = current_node.next # Move current node to next node
+
+        # If current node exists, update its value to the provided value    
+        if current_node != None:
+            current_node.data = val
+        else:
+            print("Index not present") # Else print error message
+
+def remove_first(self):
+    # Check if head does not exist, and return if head not present
+    if(self.head == None):
+        return
+    # Set head to node pointed to by the next of the the current head
+    self.head = self.head.next
+
+
