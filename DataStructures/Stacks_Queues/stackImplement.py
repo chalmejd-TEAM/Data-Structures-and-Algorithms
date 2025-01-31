8c910e6e493b512defac5f8a03bbff81942464d7
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-31 10:07:29 -0600

    Added basic stack implementation skeleton

---
+++ DataStructures/Stacks_Queues/stackImplement.py
@@ -0,0 +1,20 @@
+# Define Node class with given data and initial next value set to None
+class Node:
+    # Initialize Node with data set to given data, and next set to None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Define Stack Class
+class Stack:
+    # Initialize Stack with top and bottom set to None
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek():
+
+    def push(data):
+
+    def pop():
