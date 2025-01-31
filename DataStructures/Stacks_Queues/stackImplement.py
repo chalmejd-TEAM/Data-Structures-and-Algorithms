61cb1f9a726924aaccdb837d84cbf477eccca8ac
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-31 10:17:36 -0600

    Added peek, push, and pop methods

--- DataStructures/Stacks_Queues/stackImplement.py
+++ DataStructures/Stacks_Queues/stackImplement.py
@@ -13,8 +13,40 @@ class Stack:
         self.bottom = None
         self.length = 0
 
-    def peek():
+    def peek(self):
+        if (self.top):
+            print(self.top.data)
+        else:
+            print('Stack Empty')
 
-    def push(data):
+    def push(self, data):
+        new_node = Node(data)
+        if self.top is None:
+            self.top = new_node
+            return
+        else:
+            new_node.next = self.top
+            self.top = new_node
 
-    def pop():
+    def pop(self):
+        if self.top == None:
+            return
+        self.top = self.top.next
+
+myStack = Stack()
+
+myStack.peek()
+myStack.push('google')
+myStack.push('bing')
+myStack.push('microsoft')
+
+myStack.peek()
+
+myStack.pop()
+myStack.peek()
+
+myStack.pop()
+myStack.peek()
+
+myStack.pop()
+myStack.peek()
