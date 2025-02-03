dacb6fcdf516f76efac7dbcc5c7ed9dca3432cac
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-02-03 08:25:26 -0600

    Added graph class skeleton

---
+++ DataStructures/Graphs/graph.py
@@ -0,0 +1,67 @@
+#       2 --- 0
+#     /  \
+#    1 -- 3
+# 
+
+# Edge List
+graph = [[0, 2], [2, 3], [2, 1], [1, 3]]
+
+# Adjacent List
+graph = [[2], [2,3], [0,1,3], [1,3]]
+
+# Adjacent Matrix
+graph = {
+    0: [0,0,1,0],
+    1: [0,0,1,1],
+    2: [1,1,0,1],
+    3: [0,1,1,0]
+}
+
+class Graph:
+    def __init__(self):
+        self.numberOfNodes = 0
+        self.adjacentList = {}
+    
+    def addVertex(self, node):
+        return
+    
+    def addEdge(self, node1, node2):
+        return
+    
+    def showConnections(self):
+        allNodes = self.adjacentList.keys()
+        for node in self.allNodes:
+            nodeConnections = self.adjacentList[node]
+            connections = ''
+            for vertex in nodeConnections:
+                connections = connections + vertex + ' '
+            print(node + '-->' + connections)
+
+myGraph = Graph()
+
+myGraph.addVertex('0')
+myGraph.addVertex('1')
+myGraph.addVertex('2')
+myGraph.addVertex('3')
+myGraph.addVertex('4')
+myGraph.addVertex('5')
+myGraph.addVertex('6')
+myGraph.addEdge('3', '1')
+myGraph.addEdge('3', '4')
+myGraph.addEdge('4', '2')
+myGraph.addEdge('4', '5')
+myGraph.addEdge('1', '2')
+myGraph.addEdge('1', '0')
+myGraph.addEdge('0', '2')
+myGraph.addEdge('6', '5')
+
+myGraph.showConnections()
+
+# Answer:
+#  0-->1 2 
+#  1-->3 2 0 
+#  2-->4 1 0 
+#  3-->1 4 
+#  4-->3 2 5 
+#  5-->4 6 
+#  6-->5
