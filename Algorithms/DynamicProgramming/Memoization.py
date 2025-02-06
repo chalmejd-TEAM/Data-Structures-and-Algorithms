dcda53af9e32203fbf4db78a433a0458ea74ad6c
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-02-06 08:23:31 -0600

    Moved cache from global variable space using closures

--- Algorithms/DynamicProgramming/Memoization.py
+++ Algorithms/DynamicProgramming/Memoization.py
@@ -6,16 +6,21 @@ print(addTo80(5))
 print(addTo80(5))
 
 
-cache = {}
-def memoizedAddTo80(n):
-    if n in cache:
-        print('Using Cache...')
-        return cache[n]
-    else:
-        print('Adding to Cache...')
-        cache[n] = n+80
-        return cache[n]
+def memoizedAddTo80():
+    cache = {}
+    def closure(n):
+        nonlocal cache
+        if n in cache:
+            print('Using Cache...')
+            return cache[n]
+        else:
+            print('Adding to Cache...')
+            cache[n] = n+80
+            return cache[n]
+    return closure
 
-print(memoizedAddTo80(5))
-print(memoizedAddTo80(5))
-print(memoizedAddTo80(5))
+memoized = memoizedAddTo80()
+
+print(memoized(5))
+print(memoized(5))
+print(memoized(5))
